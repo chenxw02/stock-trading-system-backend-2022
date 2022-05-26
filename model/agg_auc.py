c44b44f56fabebffe44e5388abd3ca70dcbb3016
刘佳誉 <3200106254@zju.edu.cn>
2022-05-26 04:46:32 +0000

    实现集合竞价

--- model/agg_auc.py
+++ model/agg_auc.py
@@ -1,7 +1,16 @@
 from exts import db
 
 
-class AggAuc(db.Model):
-    @staticmethod
-    def test():
-        return
+class Instruction(db.Model):
+    __tablename__ = "instruction"
+    instruction_id = db.Column(db.String(20), nullable = False, primary_key = True)
+    stock_id = db.Column(db.String(20), db.ForeignKey("stock.stock_id"), nullable=False)
+    fund_account_number = db.Column(db.String(20), db.ForeignKey("fund_account.fund_account_number"), nullable=False)
+    buy_sell_flag = db.Column(db.Char, nullable=False)
+    target_number = db.Column(db.Integer, nullable=False)
+    actual_number = db.Column(db.Integer, nullable=False)
+    target_price = db.Column(db.Double, nullable=False)
+    time = db.Column(db.Integer(6), nullable=False)
+    instruction_state = db.Column(db.Char, nullable=False)
+    total_amount = db.Column(db.Double, nullable=False)
+
