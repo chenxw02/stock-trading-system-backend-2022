8a23b80b3e52df21ab9b40f252f25923d0867082
刘佳誉 <3200106254@zju.edu.cn>
2022-05-26 04:45:21 +0000

    update dao/agg_auc_dao.py.

--- dao/agg_auc_dao.py
+++ dao/agg_auc_dao.py
@@ -1,7 +1,27 @@
 from exts import db
-from model.agg_auc import AggAuc
+from model.agg_auc import Instruction
+from sqlalchemy import and_
+
 
 class AggAucDao:
     @staticmethod
-    def test():
-        return
+    def getbuyinstr(stock_id):
+        buy = Instruction.query.filter(and_(Instruction.buy_sell_flag == 'B', Instruction.instruction_state == 'N',
+                                            Instruction.stock_id == stock_id)).order_by('-target_price')
+        return buy
+
+    @staticmethod
+    def getsellinstr(stock_id):
+        sell = Instruction.query.filter(and_(Instruction.buy_sell_flag == 'S', Instruction.instruction_state == 'N',
+                                             Instruction.stock_id == stock_id)).order_by('-target_price')
+        sell.reserve()  # 卖价从低到高排序
+        return sell
+
+    @staticmethod
+    def getstockid():
+        record = Instruction.query.filter(and_(Instruction.instruction_state == 'N')).all()
+        s_id = []
+        for r in record:
+            if r.stock_id not in s_id:
+                s_id.append(r.stock_id)
+        return s_id
