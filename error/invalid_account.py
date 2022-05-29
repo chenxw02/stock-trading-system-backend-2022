5547d1b6e1e44568368e25b19eb4af084ed19869
A4张天涵 <10847943+a4-zhang-tianhan@user.noreply.gitee.com>
2022-05-29 03:22:44 +0000

    update error/invalid_account.py.

--- error/invalid_account.py
+++ error/invalid_account.py
@@ -1,2 +1,23 @@
 class InvalidAccountError(Exception):
     pass
+
+class InvalidAccountNameError(Exception):
+    pass
+
+class MissAccountError(Exception):
+    pass
+
+class NotSamePasswordError(Exception):
+    pass
+
+class NoneAccountNameError(Exception):
+    pass
+
+class InvalidPaymentAccountError(Exception):
+    pass
+
+class LackOfBalance(Exception):
+    pass
+
+class AlreadyAdvance(Exception):
+    pass
