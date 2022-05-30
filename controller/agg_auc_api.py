d5fad778fbbc1cb4b506b76a1f1c93d3641e41ee
刘佳誉 <3200106254@zju.edu.cn>
2022-05-30 16:24:06 +0800

    A3集合竞价整合

--- controller/agg_auc_api.py
+++ controller/agg_auc_api.py
@@ -1,13 +1,19 @@
 import json
 from flask import Blueprint, request
-from service.agg_auc_service import AggAucService
+from service.agg_auc_main import AggAucService
 from util.result import Result
 
 agg_auc_api = Blueprint('agg_auc_api', __name__)
 
+# @admin_api.route("/admin/login", methods=["POST"])
+# def login():
+#     data = json.loads(request.get_data(as_text=True))
+#     token = AdminService.login(data["admin_id"], data["password"])
+#     return Result.success(token)
+
 @agg_auc_api.route("/agg_auc", methods=["POST"])
-def register():
+def agg_auc():
     instruction_id = json.loads(request.get_data(as_text=True))
     # print(data)
-    AggAucService.register(instruction_id)
+    AggAucService.agg_auc(instruction_id)
     return Result.success(None)
