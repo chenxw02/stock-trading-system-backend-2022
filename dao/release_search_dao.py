689c8e88f325a85f700d318901690700b07195dc
PatrickX <285272812@qq.com>
2022-05-30 13:48:59 +0000

    update dao/release_search_dao.py.

--- dao/release_search_dao.py
+++ dao/release_search_dao.py
@@ -16,6 +16,7 @@ class ReleaseSearchDao:
         ret1 = db.session.query(Stock.stock_id, Stock.stock_name).filter(or_(Stock.stock_id==name, Stock.stock_name.like("%{}%".format(name)))).all()
         payload = []
         content = {}
+        print (ret1)
         for i in ret1:
             #content = {'stock_id': i[0], 'stock_name': i[1]}
             #now_date = int(datetime.datetime.now().strftime('%Y%m%d'))
@@ -25,16 +26,33 @@ class ReleaseSearchDao:
             ret2 = db.session.query(K.start_price, K.end_price, K.highest_price, K.lowest_price).filter(K.stock_id==i[0]).order_by(K.date.desc()).all()
             #ret3 = db.session.query(Transaction.buy_sell_flag, Transaction.transaction_price, Transaction.transaction_amount, Transaction.transaction_number, Transaction.transaction_date, Transaction.transaction_time).filter(Transaction.stock_id==i[0]).order_by(Transaction.transaction_date.desc()).all()
             ret3 = db.session.query(TestTransaction.buy_sell_flag, TestTransaction.transaction_price, TestTransaction.transaction_amount, TestTransaction.transaction_number, TestTransaction.transaction_date, TestTransaction.transaction_time).filter(TestTransaction.stock_id==i[0]).order_by(TestTransaction.transaction_date.desc()).all()
-            print(ret2)
-            print(ret3)
+            #print(ret2)
+            #print(ret3)
             for j in ret2:
+                count = 0
                 for k in ret3:
+                    count = count + 1
+                    if (count > 5):
+                        break
                     content = {'stock_id': i[0], 'stock_name': i[1], 'start_price': j[0], 'end_price': j[1], 'highest_price': j[2], 'lowest_price': j[3], 'buy_sell_flag': k[0], 'transaction_price': k[1], 'transaction_amount': k[2], 'transaction_number': k[3], 'transaction_date': k[4], 'transaction_time': k[5]}
                     if (len(content)==0):
                         continue
                     payload.append(content)
                     content = {}
+                if (len(ret3) == 0):
+                    content = {'stock_id': i[0], 'stock_name': i[1], 'start_price': j[0], 'end_price': j[1], 'highest_price': j[2], 'lowest_price': j[3], 'buy_sell_flag': '', 'transaction_price': '', 'transaction_amount': '', 'transaction_number': '', 'transaction_date': '', 'transaction_time': ''}
+                    if (len(content)==0):
+                        continue
+                    payload.append(content)
+                    content = {}
                 break
+            if (len(ret2) == 0 or len(ret3) == 0):
+                content = {'stock_id': i[0], 'stock_name': i[1], 'start_price': '', 'end_price': '', 'highest_price': '', 'lowest_price': '', 'buy_sell_flag': '', 'transaction_price': '', 'transaction_amount': '', 'transaction_number': '', 'transaction_date': '', 'transaction_time': ''}
+                if (len(content)==0):
+                    continue
+                payload.append(content)
+                content = {}
+
         print (payload)
         return payload
     
