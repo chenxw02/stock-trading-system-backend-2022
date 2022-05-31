ee4c34f07c072d3ebd72195c107c8587ecadaf31
刘奕骁 <liuyixiao0821@gmail.com>
2022-05-31 20:35:41 +0800

    Enable debug mode

--- config.py
+++ config.py
@@ -9,3 +9,5 @@ PORT = '3306'
 DATABASE = 'stock'
 SQLALCHEMY_DATABASE_URI = '{}+{}://{}:{}@{}:{}/{}?charset=utf8'.format(DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT,
                                                                        DATABASE)
+
+DEBUG = True
