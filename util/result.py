9146754427c4b4301cb31fd61d2c093a8b703b36
ubuntu <ubuntu@localhost.localdomain>
2022-06-06 01:52:40 +0800

    Final fix real env jsonfy error

--- util/result.py
+++ util/result.py
@@ -1,15 +1,20 @@
+from flask import jsonify
+
 class Result:
     @staticmethod
     def error(code, message):
         result = {}
-        result["code"] = code
-        result["message"] = message
-        return result
+        result["code"] = str(code)
+        result["message"] = str(message)
+        return jsonify(result)
 
     @staticmethod
     def success(data):
         result = {}
-        result["code"] = 0
+        result["code"] = "0"
         result["message"] = "success"
-        result["data"] = data
-        return result
+        if isinstance(data, bytes):
+            result["data"] = str(data, 'utf-8')
+        else:
+            result["data"] = data
+        return jsonify(result)
